5274bba6e2155ca73c00f1344b09ab87223c5241
Timenem <https://github.com/Timenem>
2021-10-13 18:50:23 +0300

    added Reader

---
+++ Reader.java
@@ -0,0 +1,21 @@
+package Lesson10;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Reader {
+    public static void main(String[] args) throws IOException {
+        File readFile = new File("/home/hind/Desktop/java_proj/iteco/src/main/java/Lesson10/outFile.txt");
+        FileInputStream fist = new FileInputStream(readFile); //create input stream
+        int size = fist.available(); //create arrays for read
+        byte[] content = new byte[size]; //return len arrays
+        fist.read(content);// read content
+        StringBuilder sb = new StringBuilder();
+        for (byte b : content)
+            sb.append((char)b);
+
+        System.out.println(sb.toString());
+    }
+}
