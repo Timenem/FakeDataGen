d42685fe69c68eba21a70d4533834d490d29b5bf
Timenem <https://github.com/Timenem>
2021-10-13 18:51:00 +0300

    added Writer

---
+++ Writer.java
@@ -0,0 +1,24 @@
+package Lesson10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class Writer {
+    public static void main(String[] args) throws IOException {
+        File writeFile = new File("/home/hind/Desktop/java_proj/iteco/src/main/java/Lesson10/outFile.txt");
+        List<String> content = Arrays.asList("7 ","fridays ","in " , "one week"); //create object
+        FileOutputStream fileOutputStream = new FileOutputStream(writeFile);  //create out stream
+        for (String str : content){
+            byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+            fileOutputStream.write(bytes);//write content
+        }
+        fileOutputStream.flush(); //clean buffer
+        fileOutputStream.close(); //close stream
+    }
+    }
